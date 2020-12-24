c7e3cb92709b430b28bcb2a6ccf235ae5e6496c6
Juanlu <jozaez@gmail.com>
2020-12-24 13:27:48 +0100

    Refactor

--- library/rabbitmq/src/test/kotlin/acceptance/RabbitPublishSubscribeFlowTest.kt
+++ library/rabbitmq/src/test/kotlin/acceptance/RabbitPublishSubscribeFlowTest.kt
@@ -1,31 +1,32 @@
-package com.home.kafka
+package acceptance
 
 import com.home.messagebroker.DomainEvent
 import com.home.messagebroker.DomainEventName
 import com.home.messagebroker.DomainEventSubscriber
 import com.home.messagebroker.DomainSubscriberRegistry
+import com.home.rabbitmq.RabbitConsumerRegisterer
+import com.home.rabbitmq.RabbitProducer
 import org.awaitility.Awaitility.await
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
 import kotlin.reflect.KClass
 
-class SubscriberTest {
+class RabbitPublishSubscribeFlowTest {
 
     private val subscriber1 = TestSubscriber1()
     private val subscriber2 = TestSubscriber2()
 
     @AfterEach
     fun tearDown() {
-        KafkaTestUtils.deleteTopic("test.event")
-        KafkaTestUtils.deleteConsumers()
+        RabbitAMPQTestUtils.deleteQueue(subscriber1.name(), subscriber2.name())
     }
 
     @Test
     fun `it raises event and subscribers are executed`() {
         DomainSubscriberRegistry.register(subscriber1, subscriber2)
-        KafkaConsumerRegisterer().registerSubscribers()
+        RabbitConsumerRegisterer().registerSubscribers()
 
-        KafkaPublisher().publish(TestDomainEvent("id"))
+        RabbitProducer().publish(TestDomainEvent("id"))
 
         await().until {
             subscriber1.executions == 1 && subscriber2.executions == 1
@@ -37,7 +38,6 @@ class SubscriberTest {
 class TestSubscriber1 : DomainEventSubscriber<TestDomainEvent>() {
     var executions = 0
     override fun on(event: TestDomainEvent) {
-        println("passed subscriber 1")
         executions++
     }
 
@@ -49,7 +49,6 @@ class TestSubscriber1 : DomainEventSubscriber<TestDomainEvent>() {
 class TestSubscriber2 : DomainEventSubscriber<TestDomainEvent>() {
     var executions = 0
     override fun on(event: TestDomainEvent) {
-        println("passed subscriber 2")
         executions++
     }
 
